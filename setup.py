05f601bcadbf67bda8237d379029d3fe72de7776
Michael Rushton <m.rushton@imperial.ac.uk>
2013-12-29 18:45:00 +0000

    Now builds

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 import sys
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 from setuptools.extension import Extension
 
-import Cython
-from Cython.Distutils import build_ext
-from Cython.Build import cythonize
-
-cython_cexprtk = Extension("cexprtk",
-  ["cexprtk.pyx"],
-  include_dirs = ["3rdparty/exprtk"])
-
-# Create list of webresources files within fitting
+try:
+  from Cython.Distutils import build_ext
+  from Cython.Build import cythonize
+except ImportError:
+  cython_cexprtk = Extension("cexprtk",
+                             ["cython/cexprtk.cpp"],
+                             language = "c++",
+                             include_dirs = ["3rdparty/exprtk", "cython"])
+  ext_modules = [ cython_cexprtk ]
+  cmdclass = {}
+else:
+    cython_cexprtk = Extension("cexprtk",
+      ["cython/cexprtk.pyx"],
+      include_dirs = ["3rdparty/exprtk", "cython"])
+    ext_modules = cythonize([cython_cexprtk])
+    cmdclass = { 'build_ext': build_ext }
 
 setup(name="cexprtk",
   description = "Mathematical expression parser: cython wrapper around the 'C++ Mathematical Expression Toolkit Library' ",
   author = "M.J.D. Rushton",
   author_email = "m.j.d.rushton@gmail.com",
-  version = 0.1.0,
-  setup_requires = ['cython>=0.16'],
-  ext_modules = cythonize([ cython_cexprtk ]))
-
+  version = "0.1.0",
+  ext_modules = ext_modules)
