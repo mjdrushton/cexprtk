767d62dd58132ad9bde12f232ecc322a4ca7723e
Michael Rushton <m.j.d.rushton@gmail.com>
2013-12-31 08:48:59 +0000

    Rebuilding from cython sources reinstated within setup.py (found cmdclass argument was missing from call to setup()).

--- setup.py
+++ setup.py
@@ -4,33 +4,27 @@ import os
 from setuptools import setup
 from setuptools.extension import Extension
 
-# Uncomment to rebuild from cython .pyx sources
-# try:
-#   from Cython.Distutils import build_ext
-#   from Cython.Build import cythonize
-# except ImportError:
-#   cython_cexprtk = Extension("cexprtk",
-#                              ["cython/cexprtk.cpp"],
-#                              language = "c++",
-#                              include_dirs = ["3rdparty/exprtk", "cython"])
-#   ext_modules = [ cython_cexprtk ]
-  # cmdclass = {}
-# else:
-#     cython_cexprtk = Extension("cexprtk",
-#       ["cython/cexprtk.pyx"],
-#       include_dirs = ["3rdparty/exprtk", "cython"])
-#     ext_modules = cythonize([cython_cexprtk])
-#     cmdclass = { 'build_ext': build_ext }
+try:
+  from Cython.Distutils import build_ext
+  from Cython.Build import cythonize
+except ImportError:
+  cython_cexprtk = Extension("cexprtk",
+                             ["cython/cexprtk.cpp"],
+                             language = "c++",
+                             include_dirs = ["3rdparty/exprtk", "cython"])
+  ext_modules = [ cython_cexprtk ]
+  cmdclass = {}
+else:
+    cython_cexprtk = Extension("cexprtk",
+      ["cython/cexprtk.pyx"],
+      include_dirs = ["3rdparty/exprtk", "cython"])
+    ext_modules = cythonize([cython_cexprtk])
+    cmdclass = { 'build_ext': build_ext }
 
-cython_cexprtk = Extension("cexprtk",
-                           ["cython/cexprtk.cpp"],
-                           language = "c++",
-                           include_dirs = ["3rdparty/exprtk", "cython"])
-ext_modules = [ cython_cexprtk ]
-cmdclass = {}
 
 setup(name="cexprtk",
   ext_modules = ext_modules,
+  cmdclass = cmdclass,
   test_suite = "tests",
   description = "Mathematical expression parser: cython wrapper around the 'C++ Mathematical Expression Toolkit Library' ",
   long_description = open('README.rst').read(),
