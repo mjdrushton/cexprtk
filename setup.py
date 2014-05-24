4c6be283762d8f2c43ae518773b05d2a33d50937
M.J.D. Rushton <M.J.D.Rushton@gmail.com>
2014-05-24 23:56:59 +0100

    Modified setup.py to support building under windows.

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
 import sys
 import os
-
+import platform
 from setuptools import setup
 from setuptools.extension import Extension
 
-# Uncomment to rebuild from cython .pyx sources
-# try:
-#   from Cython.Distutils import build_ext
-#   from Cython.Build import cythonize
-# except ImportError:
-#   cython_cexprtk = Extension("cexprtk",
-#                              ["cython/cexprtk.cpp"],
-#                              language = "c++",
-#                              include_dirs = ["3rdparty/exprtk", "cython"])
-#   ext_modules = [ cython_cexprtk ]
-  # cmdclass = {}
-# else:
-#     cython_cexprtk = Extension("cexprtk",
-#       ["cython/cexprtk.pyx"],
-#       include_dirs = ["3rdparty/exprtk", "cython"])
-#     ext_modules = cythonize([cython_cexprtk])
-#     cmdclass = { 'build_ext': build_ext }
+defines =[]
+if platform.system() == 'Windows':
+  defines.append(('WIN32', None))
 
-cython_cexprtk = Extension("cexprtk",
-                           ["cython/cexprtk.cpp"],
-                           language = "c++",
-                           include_dirs = ["3rdparty/exprtk", "cython"])
-ext_modules = [ cython_cexprtk ]
-cmdclass = {}
+# Uncomment to rebuild from cython .pyx sources
+try:
+  from Cython.Distutils import build_ext
+  from Cython.Build import cythonize
+except ImportError:
+  ext_modules= [Extension("cexprtk",
+      ["cython/cexprtk.cpp"],
+      language = "c++",
+      include_dirs = ["3rdparty/exprtk", "cython"],
+      define_macros = defines) ]
+else:
+  cmdclass = { 'build_ext': build_ext }
+  cython_cexprtk = Extension("cexprtk",
+    ["cython/cexprtk.pyx"],
+    include_dirs = ["3rdparty/exprtk", "cython"],
+    define_macros = defines)
+  ext_modules = cythonize([cython_cexprtk])
 
 setup(name="cexprtk",
   ext_modules = ext_modules,
@@ -36,10 +33,10 @@ setup(name="cexprtk",
   long_description = open('README.rst').read(),
   author = "M.J.D. Rushton",
   author_email = "m.j.d.rushton@gmail.com",
-  version = "0.1.1",
+  version = "0.1.2",
   license = "CPL",
   url = "https://bitbucket.org/mjdr/cexprtk",
-  download_url = "https://bitbucket.org/mjdr/cexprtk/get/0.1.1.tar.gz",
+  download_url = "https://bitbucket.org/mjdr/cexprtk/get/0.1.2.tar.gz",
   keywords = ["math", "formula", "parser", "arithmetic", "evaluate"],
   classifiers = [
     "License :: OSI Approved :: Common Public License",
