dee1b0a8b2ca1e4499326c29b7efd1c6ab7eb0eb
M.J.D. Rushton <m.j.d.rushton@gmail.com>
2019-12-08 23:14:12 +0000

    Updated the build wheels script for windows.

--- build-wheels/build-windows.bat
+++ build-wheels/build-windows.bat
@@ -1,34 +1,61 @@
 REM Python 2.7
 IF NOT exist venv-27 (
-    pip2.7 install --upgrade pip
-    pip2.7 install virtualenv
+    pip2.7 install --user --upgrade pip
+    pip2.7 install --user virtualenv
     virtualenv -p c:\python27\python.exe venv-27
 )
 
 call venv-27\Scripts\activate
-pip install wheel nose
+pip install wheel
 pip wheel . -w wheelhouse
 call venv-27\Scripts\deactivate
 
-REM Python 3.6
-IF NOT exist venv-36 (
-    virtualenv -p "c:\Program Files\Python36\python.exe" venv-36
+
+REM Python 2.7 32 bit
+IF NOT exist venv-27-32 (
+    pip2.7 install --user --upgrade pip
+    pip2.7 install --user virtualenv
+    virtualenv -p c:\python27-32\python.exe venv-27-32
 )
 
-call venv-36\Scripts\activate
-pip install wheel nose
+call venv-27-32\Scripts\activate
+pip install wheel
 pip wheel . -w wheelhouse
-call venv-36\Scripts\deactivate
+call venv-27-32\Scripts\deactivate
 
-REM Python 3.5
+
+REM REM Python 3.5
 IF NOT exist venv-35 (
     virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python35\python.exe" venv-35
 )
 
 call venv-35\Scripts\activate
-pip install wheel nose
+pip install wheel
 pip wheel . -w wheelhouse
 call venv-35\Scripts\deactivate
+
+
+REM Python 3.6
+IF NOT exist venv-36 (
+    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python36\python.exe" venv-36
+)
+
+call venv-36\Scripts\activate
+pip install wheel
+pip wheel . -w wheelhouse
+call venv-36\Scripts\deactivate
+
+
+REM Python 3.6 32 bit
+IF NOT exist venv-36-32 (
+    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python36-32\python.exe" venv-36-32
+)
+
+call venv-36-32\Scripts\activate
+pip install wheel
+pip wheel . -w wheelhouse
+call venv-36-32\Scripts\deactivate
+
 
 REM Python 3.7
 IF NOT exist venv-37 (
@@ -36,29 +63,39 @@ IF NOT exist venv-37 (
 )
 
 call venv-37\Scripts\activate
-pip install wheel nose
+pip install wheel
 pip wheel . -w wheelhouse
 call venv-37\Scripts\deactivate
 
+REM Python 3.7 32 bit
+IF NOT exist venv-37-32 (
+    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python37-32\python.exe" venv-37-32
+)
 
-REM Tests
-call venv-27\Scripts\activate
-pip -v install cexprtk --no-cache-dir --no-index -f wheelhouse
-nosetests cexprtk
-call venv-27\Scripts\deactivate
+call venv-37-32\Scripts\activate
+pip install wheel
+pip wheel . -w wheelhouse
+call venv-37-32\Scripts\deactivate
 
-call venv-35\Scripts\activate
-pip -v install cexprtk --no-cache-dir --no-index -f wheelhouse
-nosetests cexprtk
-call venv-35\Scripts\deactivate
 
-call venv-36\Scripts\activate
-pip -v install cexprtk --no-cache-dir --no-index -f wheelhouse
-nosetests cexprtk
-call venv-36\Scripts\deactivate
+REM Python 3.8
+IF NOT exist venv-38 (
+    virtualenv -p "c:\Python38\python.exe" venv-38
+)
 
-call venv-37\Scripts\activate
-pip -v install cexprtk --no-cache-dir --no-index -f wheelhouse
-nosetests cexprtk
-call venv-37\Scripts\deactivate
+call venv-38\Scripts\activate
+pip install wheel
+pip wheel . -w wheelhouse
+call venv-38\Scripts\deactivate
+
+
+REM Python 3.8 32 bit
+IF NOT exist venv-38-32 (
+    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python38-32\python.exe" venv-38-32
+)
+
+call venv-38-32\Scripts\activate
+pip install wheel
+pip wheel . -w wheelhouse
+call venv-38-32\Scripts\deactivate
 
