dcf42a7ed491038af4640aa27ab1c1f3ea7eadfc
Michael Rushton <m.j.d.rushton@gmail.com>
2019-12-08 21:11:55 +0000

    Updated build-macosx.sh script for building wheels on mac.

--- build-wheels/build-macosx.sh
+++ build-wheels/build-macosx.sh
@@ -9,10 +9,10 @@ SCRIPT_DIR="$(cd "$(dirname $0)";$(which pwd))"
 ROOT_DIR="$SCRIPT_DIR/.."
 
 cd "$ROOT_DIR"
-for PE in 2.7.15 3.3.7 3.4.8 3.5.5 3.6.5 3.7.0;do
+for PE in 2.7.17 3.5.8 3.6.9 3.7.5 3.8.0;do
   pyenv local $PE
   PIP="$(pyenv which pip)"
   "$PIP" install --upgrade pip
-  "$PIP" install wheel
+  "$PIP" install wheel m2r
   "$PIP" wheel . -w wheelhouse/
 done
