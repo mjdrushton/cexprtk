8bee3207130e092eab5f8391a2dc1de6af2ef06f
mjdrushton <m.j.d.rushton@gmail.com>
2021-12-11 23:52:42 +0000

    Update build-windows.bat
    Updated windows script to build wheels. Dropped python 2.7 support and added 3.9 and 3.10. Also dropped building of 32-bit wheels on windows.

--- build-wheels/build-windows.bat
+++ build-wheels/build-windows.bat
@@ -1,110 +1,57 @@
-REM Python 2.7
-IF NOT exist venv-27 (
-    pip2.7 install --user --upgrade pip
-    pip2.7 install --user virtualenv
-    virtualenv -p c:\python27\python.exe venv-27
-)
-
-call venv-27\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
-pip install wheel
-pip wheel . -w wheelhouse
-call venv-27\Scripts\deactivate
-
-
-REM Python 2.7 32 bit
-IF NOT exist venv-27-32 (
-    pip2.7 install --user --upgrade pip
-    pip2.7 install --user virtualenv
-    virtualenv -p c:\python27-32\python.exe venv-27-32
-)
-
-call venv-27-32\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
-pip install wheel
-pip wheel . -w wheelhouse
-call venv-27-32\Scripts\deactivate
-
-
-REM REM Python 3.5
-IF NOT exist venv-35 (
-    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python35\python.exe" venv-35
-)
-
-call venv-35\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
-pip install wheel
-pip wheel . -w wheelhouse
-call venv-35\Scripts\deactivate
-
 
 REM Python 3.6
 IF NOT exist venv-36 (
-    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python36\python.exe" venv-36
+    py -3.6 -m venv venv-36
 )
 
 call venv-36\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
+pip install --upgrade "setuptools>=38.6.0"
 pip install wheel
 pip wheel . -w wheelhouse
 call venv-36\Scripts\deactivate
 
-
-REM Python 3.6 32 bit
-IF NOT exist venv-36-32 (
-    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python36-32\python.exe" venv-36-32
-)
-
-call venv-36-32\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
-pip install wheel
-pip wheel . -w wheelhouse
-call venv-36-32\Scripts\deactivate
-
-
 REM Python 3.7
 IF NOT exist venv-37 (
-    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python37\python.exe" venv-37
+    py -3.7 -mvenv venv-37
 )
 
 call venv-37\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
+pip install --upgrade "setuptools>=38.6.0"
 pip install wheel
 pip wheel . -w wheelhouse
 call venv-37\Scripts\deactivate
 
-REM Python 3.7 32 bit
-IF NOT exist venv-37-32 (
-    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python37-32\python.exe" venv-37-32
-)
-
-call venv-37-32\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
-pip install wheel
-pip wheel . -w wheelhouse
-call venv-37-32\Scripts\deactivate
-
-
 REM Python 3.8
 IF NOT exist venv-38 (
-    virtualenv -p "c:\Python38\python.exe" venv-38
+    py -3.8 -mvenv venv-38
 )
 
 call venv-38\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
+pip install --upgrade "setuptools>=38.6.0"
 pip install wheel
 pip wheel . -w wheelhouse
 call venv-38\Scripts\deactivate
 
 
-REM Python 3.8 32 bit
-IF NOT exist venv-38-32 (
-    virtualenv -p "%USERPROFILE%\AppData\Local\Programs\Python\Python38-32\python.exe" venv-38-32
+REM Python 3.9
+IF NOT exist venv-39 (
+    py -3.9 -mvenv venv-39
 )
 
-call venv-38-32\Scripts\activate
-pip install --upgrade setuptools>=38.6.0
+call venv-39\Scripts\activate
+pip install --upgrade "setuptools>=38.6.0"
 pip install wheel
 pip wheel . -w wheelhouse
-call venv-38-32\Scripts\deactivate
+call venv-39\Scripts\deactivate
+
+REM Python 3.10
+IF NOT exist venv-310 (
+    py -3.10 -mvenv venv-310
+)
+
+call venv-310\Scripts\activate
+pip install --upgrade "setuptools>=38.6.0"
+pip install wheel
+pip wheel . -w wheelhouse
+call venv-310\Scripts\deactivate
 
