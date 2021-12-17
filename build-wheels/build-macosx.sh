4201aaad4f72ce81edd85d043f6391209255a844
Michael Rushton <m.j.d.rushton@gmail.com>
2021-12-17 21:53:46 +0000

    Updated python versions in script to build wheels
    Python versions updated in macosx script.

--- build-wheels/build-macosx.sh
+++ build-wheels/build-macosx.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$(cd "$(dirname $0)";$(which pwd))"
 ROOT_DIR="$SCRIPT_DIR/.."
 
 cd "$ROOT_DIR"
-for PE in 2.7.17 3.5.8 3.6.9 3.7.5 3.8.0;do
+for PE in 3.9.9 3.6.10 3.7.12 3.8.12;do
   pyenv local $PE
   PIP="$(pyenv which pip)"
   "$PIP" install --upgrade pip
